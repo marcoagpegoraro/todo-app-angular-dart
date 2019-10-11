cdb8688ab536e8f914dfb300abdb3f3ba526c035
Marco Antonio Gonçalves Pegoraro <magpegoraro@ibm.com>
2019-10-11 12:21:16 -0300

    Angular version 5.2.0

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,7 +2,12 @@ import 'package:angular/angular.dart';
 
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  // styleUrls: const ['app_component.css'],
+  templateUrl: 'app_component.html',
+  // TODO: Remove MaterialIconComponent from directives.
+  // (Blocked on https://github.com/dart-lang/angular_components/issues/145.)
+  // directives: const [CORE_DIRECTIVES, materialDirectives, MaterialIconComponent],
+  // providers: const [materialProviders],
 )
 class AppComponent {
   var name = 'Angular';
