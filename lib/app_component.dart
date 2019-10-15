823ef12aa8f8f453b0a20555ac4f4df5b4194797
Marco Antonio Gonçalves Pegoraro <magpegoraro@ibm.com>
2019-10-15 09:49:30 -0300

    Adicionado estilos material design

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,14 +1,41 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+import 'package:angular_router/angular_router.dart';
 
 @Component(
   selector: 'my-app',
-  // styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html',
-  // TODO: Remove MaterialIconComponent from directives.
-  // (Blocked on https://github.com/dart-lang/angular_components/issues/145.)
-  // directives: const [CORE_DIRECTIVES, materialDirectives, MaterialIconComponent],
-  // providers: const [materialProviders],
+  styleUrls: [
+    'app_component.css',
+    'package:angular_components/app_layout/layout.scss.css',
+  ],
+  directives: [
+    coreDirectives,
+    routerDirectives,
+    DeferredContentDirective,
+    MaterialButtonComponent,
+    MaterialIconComponent,
+    MaterialPersistentDrawerDirective,
+    MaterialToggleComponent,
+    MaterialListComponent,
+    MaterialListItemComponent
+  ],
+  providers: [
+    materialProviders,
+    ExistingProvider(DeferredContentAware, MaterialTemporaryDrawerComponent)
+  ],
 )
-class AppComponent {
-  var name = 'Angular';
+class AppComponent extends MaterialDrawerBase {
+  AppComponent() : super(visible: false);
+  var name = 'World';
+
+  @HostListener('click')
+  @override
+  void toggle() {
+    if (visible && !canClose) return;
+    super.toggle();
+  }
+
+  @Input()
+  bool canClose = true;
 }
