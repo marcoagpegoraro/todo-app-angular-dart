aa087ed1e852f63993f49c46e67647cffff48988
Marco Antonio Gonçalves Pegoraro <magpegoraro@ibm.com>
2019-10-15 15:55:10 -0300

    Colocado o bulma, porem o nav bar n ta responsivel.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,5 +1,4 @@
 import 'package:angular/angular.dart';
-import 'package:angular_components/angular_components.dart';
 import 'package:angular_router/angular_router.dart';
 
 @Component(
@@ -7,35 +6,12 @@ import 'package:angular_router/angular_router.dart';
   templateUrl: 'app_component.html',
   styleUrls: [
     'app_component.css',
-    'package:angular_components/app_layout/layout.scss.css',
   ],
   directives: [
     coreDirectives,
     routerDirectives,
-    DeferredContentDirective,
-    MaterialButtonComponent,
-    MaterialIconComponent,
-    MaterialPersistentDrawerDirective,
-    MaterialToggleComponent,
-    MaterialListComponent,
-    MaterialListItemComponent
-  ],
-  providers: [
-    materialProviders,
-    ExistingProvider(DeferredContentAware, MaterialTemporaryDrawerComponent)
   ],
 )
-class AppComponent extends MaterialDrawerBase {
-  AppComponent() : super(visible: false);
-  var name = 'World';
-
-  @HostListener('click')
-  @override
-  void toggle() {
-    if (visible && !canClose) return;
-    super.toggle();
-  }
-
-  @Input()
-  bool canClose = true;
+class AppComponent {
+  var name = 'Marco';
 }
